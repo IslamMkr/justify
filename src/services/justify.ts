79d5652b3f3df8a3cf37a990b41088d310abe2d0
Islam Mokrane <islammkr01@gmail.com>
2024-10-15 18:04:41 +0200

    refactor: improve text justification algorithm and handle consecutive newlines

--- src/services/justify.ts
+++ src/services/justify.ts
@@ -1,30 +1,34 @@
 /**
  * Justifies a given text to fit within a specified line length.
+ * Handles consecutive newlines by reducing them to one and preserves paragraph structure.
  *
  * @param text - The text to be justified.
  * @param lineLength - The maximum length of each line. Defaults to 80 characters.
  * @returns {string} The justified text with each line fitting within the specified length.
  */
-export const justifyText = (text: string, lineLength: number = 80) => {
-	const words = text.split(" ")
-	let currentLine = ""
+export const justifyText = (text: string, lineLength: number = 80): string => {
+	const sanitizedText = text.replace(/\n+/g, "\n")
+	const paragraphs = sanitizedText.split("\n")
 	let justifiedText = ""
 
-	words.forEach((word) => {
-		if ((currentLine + word).length < lineLength) {
-			currentLine += word + " "
-		} else if ((currentLine + word).length > lineLength) {
-			justifiedText += justifyLine(currentLine.trim(), lineLength) + "\n"
-			currentLine = word + " "
-		} else {
-			justifiedText += currentLine + word + "\n"
-			currentLine = ""
-		}
+	paragraphs.forEach((paragraph) => {
+		const words = paragraph.split(" ")
+		let currentLine = ""
+
+		words.forEach((word) => {
+			if ((currentLine + word).length < lineLength) {
+				currentLine += word + " "
+			} else if ((currentLine + word).length > lineLength) {
+				justifiedText += justifyLine(currentLine.trim(), lineLength) + "\n"
+				currentLine = word + " "
+			} else {
+				justifiedText += currentLine + word + "\n"
+				currentLine = ""
+			}
+		})
+
+		justifiedText += currentLine.trim() + "\n"
 	})
-
-	justifiedText += currentLine
-
-	console.log(justifiedText)
 
 	return justifiedText.trim()
 }
@@ -36,11 +40,15 @@ export const justifyText = (text: string, lineLength: number = 80) => {
  * @param lineLength - The desired length of the justified line.
  * @returns {string} The justified line of text.
  */
-const justifyLine = (line: string, lineLength: number) => {
+const justifyLine = (line: string, lineLength: number): string => {
 	const words = line.split(" ")
+
+	if (words.length === 1) {
+		return words[0]
+	}
+
 	const totalSpaces = lineLength - words.join("").length
 	const spacesPerWord = Math.floor(totalSpaces / (words.length - 1))
-
 	let spacingLeft = totalSpaces - spacesPerWord * (words.length - 1)
 	let justifiedLine = ""
 
